78da53e37d2a47d81eab5257af19887f75f3ac2e
John Johnson <jjmbeba@gmail.com>
2023-01-30 02:07:03 +0300

    updated server url

--- Client/components/Home.jsx
+++ Client/components/Home.jsx
@@ -25,7 +25,7 @@ const HomeSection = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post',{
+                const response = await fetch('https://dall-e-snw0.onrender.com/api/v1/post',{
                     method:"GET",
                     headers:{
                         'Content-Type':'application/json'
