3cada2f84c6dc39a216b9542ae68649f6becffbe
John Johnson <jjmbeba@gmail.com>
2023-01-30 02:14:39 +0300

    fix home

--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link' 
 import Header from '../components/Header'
-import HomeSection from '../components/home'
+import HomeSection from '../components/home.jsx'
 
 export default function Home() {
   return (
